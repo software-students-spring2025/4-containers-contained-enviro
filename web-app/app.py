f311cc766b165bde3c2e7188a30d6ac4a1aeba09
jackinthebox314 <jwangjy315@gmail.com>
2025-04-07 18:35:51 -0400

    begin work on web-app

---
+++ web-app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import pymongo
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.secret_key = os.getenv("SECRET_KEY")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5001), debug=True)
