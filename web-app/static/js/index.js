ca7f582a26fd31eeb1ebac83a0e7bd993abe4cb6
jackinthebox314 <jwangjy315@gmail.com>
2025-04-08 14:52:13 -0400

    increased feedback

--- web-app/static/js/index.js
+++ web-app/static/js/index.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded',(evt) => {
     console.log("Hello world")
     const record_btn = document.querySelector('.record-btn')
-    record_btn.id = 'start-recording'
+    const expand_btn = document.querySelector('.expand-btn')
+    record_btn.id = 'stop-recording'
     record_btn.addEventListener('click',(evt) => {
         console.log("clicked button!")
         if (record_btn.id == 'stop-recording') {
-            record_btn.innerHTML = '<ion-icon name="mic-outline"></ion-icon>'
-            record_btn.id = 'start-recording'
-        } else {
             record_btn.innerHTML = '<ion-icon name="pause-outline"></ion-icon>'
+            record_btn.id = 'start-recording'
+            expand_btn.id = 'start-expand'
+        } else {
+            record_btn.innerHTML = '<ion-icon name="mic-outline"></ion-icon>'
             record_btn.id = 'stop-recording'
+            expand_btn.id = 'stop-expand'
         }
     })
 })
