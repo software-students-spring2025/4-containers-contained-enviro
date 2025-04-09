984fff3fc339ba1ba087fa41fa3b82654c6bcbe1
jackinthebox314 <jwangjy315@gmail.com>
2025-04-09 19:43:06 -0400

    removed speech recognition functionality from machine learning

--- machine-learning-client/ml.py
+++ machine-learning-client/ml.py
@@ -1,5 +1,4 @@
-"""Machine learning module for speech recognition and similarity prediction."""
-import speech_recognition as sr
+"""Machine learning client for similarity recommendation."""
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
@@ -20,31 +19,3 @@ def get_recommendations_from_description(description, tfidf, tfidf_matrix, df, t
     result_df['similarity'] = [score for _, score in sim_scores]
 
     return result_df[['title', 'similarity', 'description']]
-
-recognizer = sr.Recognizer()
-
-# print("Available microphones:")
-# print(sr.Microphone.list_microphone_names())
-try:
-    with sr.Microphone() as source:
-        print("Adjusting noise...")
-        recognizer.adjust_for_ambient_noise(source, duration=0.5)
-        print("Recording for 4 seconds...")
-        recorded_audio = recognizer.listen(source, timeout=4)
-        print("Done recording.")
-except sr.WaitTimeoutError:
-    print("listening timed out while waiting for phrase to start")
-
-try:
-    print("Recognizing the text...")
-    text = recognizer.recognize_google(recorded_audio, language="en-US")
-    text = text.lower()
-    print(f"Decoded Text: {text}")
-    recommendations = get_recommendations_from_description(text, v, v_matrix, movies)
-    print(recommendations)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand the audio.")
-except sr.RequestError:
-    print("Could not request results from Google Speech Recognition service.")
-except NameError as ne:
-    print(ne)
