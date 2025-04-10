b0342072086ace56b15d34964d983d1bba6b85dc
jackinthebox314 <jwangjy315@gmail.com>
2025-04-09 20:54:57 -0400

    base flask app

--- web-app/app.py
+++ web-app/app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+"""Flask app"""
+ # pylint: disable=unused-import, method-hidden
+ # pylint: enable=too-many-lines
 import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+# from flask_login import (
+#     LoginManager,
+#     UserMixin,
+#     login_user,
+#     logout_user,
+#     login_required,
+#     current_user,
+# )
 
-app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app = Flask(
+    __name__, template_folder="templates", static_folder="static", static_url_path="/"
+)
 app.secret_key = os.getenv("SECRET_KEY")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    """Default path"""
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5001), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5001), debug=True)
