f1b0267131fc5b67989da65451ac4affb3dc54d6
joylyngong <128427079+joylyngong@users.noreply.github.com>
2025-04-10 04:24:32 -0400

    Create test_routes.py

---
+++ web-app/test/test_routes.py
@@ -0,0 +1,8 @@
+"""Unit tests for Flask routes."""
+
+
+def test_index_route(test_client):
+    """Test the home route returns the correct response."""
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert b"Flask works" in response.data
