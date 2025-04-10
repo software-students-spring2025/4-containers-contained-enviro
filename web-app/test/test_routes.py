74d177602dfd262c3beffdd9e18755ff414720e7
joylyngong <128427079+joylyngong@users.noreply.github.com>
2025-04-10 02:58:20 -0400

    Update test_routes.py

--- web-app/test/test_routes.py
+++ web-app/test/test_routes.py
@@ -1,4 +1,7 @@
+
+
 def test_index(client):
+    """Test the root route '/' returns a 200 OK and expected content."""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Flask works" in response.data
