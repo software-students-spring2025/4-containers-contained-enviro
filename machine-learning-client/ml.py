a7f70de3e89491465a49388ae373ce9c76bf9100
jackinthebox314 <jwangjy315@gmail.com>
2025-04-09 20:28:12 -0400

    completed testing and modifying of ml.py

--- machine-learning-client/ml.py
+++ machine-learning-client/ml.py
@@ -2,27 +2,25 @@
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-import pandas as pd
 
 
 class MLC:  # pylint: disable=too-few-public-methods
     """This is the Machine Learning Client class"""
 
-    def __init__(self, data):
+    def __init__(self, descriptions):
         """Takes data and fits a tf-idf vectorizer with it"""
-        self.data = pd.DataFrame(data)
         self.tfidf = TfidfVectorizer()
-        self.tfidf_matrix = self.tfidf.fit_transform(data)
+        self.tfidf_matrix = self.tfidf.fit_transform(descriptions)
 
     def get_recommendations(self, description, df, threshold=0.1):
         """Returns movies sorted by similarity to provided description."""
+        print(df)
         desc_vector = self.tfidf.transform([description])
         cosine_sim = linear_kernel(desc_vector, self.tfidf_matrix).flatten()
         sim_scores = [
             (i, score) for i, score in enumerate(cosine_sim) if score >= threshold
         ]
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
         result_df = df.iloc[[i for i, _ in sim_scores]].copy()
         result_df["similarity"] = [score for _, score in sim_scores]
 
