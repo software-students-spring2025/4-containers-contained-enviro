2ac68e213542b853be9f952aafb4cc56148c01ff
jackinthebox314 <jwangjy315@gmail.com>
2025-04-09 20:05:19 -0400

    restructured machine learning client

--- machine-learning-client/ml.py
+++ machine-learning-client/ml.py
@@ -4,24 +4,26 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
 
-movies = pd.read_csv("movies.csv")
+class MLC: # pylint: disable=too-few-public-methods
+    """This is the Machine Learning Client class"""
+    def __init__(self, data):
+        """Takes data and fits a tf-idf vectorizer with it"""
+        self.data = pd.DataFrame(data)
+        self.tfidf = TfidfVectorizer()
+        self.tfidf_matrix = self.tfidf.fit_transform(data)
 
-v = TfidfVectorizer()
-v_matrix = v.fit_transform(movies["description"].values.astype("U"))
+    def get_recommendations(
+        self, description, df, threshold=0.1
+    ):
+        """Returns movies sorted by similarity to provided description."""
+        desc_vector = self.tfidf.transform([description])
+        cosine_sim = linear_kernel(desc_vector, self.tfidf_matrix).flatten()
+        sim_scores = [
+            (i, score) for i, score in enumerate(cosine_sim) if score >= threshold
+        ]
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
+        result_df = df.iloc[[i for i, _ in sim_scores]].copy()
+        result_df["similarity"] = [score for _, score in sim_scores]
 
-def get_recommendations_from_description(
-    description, tfidf, tfidf_matrix, df, threshold=0.1
-):
-    """Returns movies sorted by similarity to provided description."""
-    desc_vector = tfidf.transform([description])
-    cosine_sim = linear_kernel(desc_vector, tfidf_matrix).flatten()
-    sim_scores = [
-        (i, score) for i, score in enumerate(cosine_sim) if score >= threshold
-    ]
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    result_df = df.iloc[[i for i, _ in sim_scores]].copy()
-    result_df["similarity"] = [score for _, score in sim_scores]
-
-    return result_df[["title", "similarity", "description"]]
+        return result_df[["title", "similarity", "description"]]
