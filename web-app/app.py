5555c05b3d1ccfe7fefd0526e63832e8a1eb1f98
jackinthebox314 <jwangjy315@gmail.com>
2025-04-09 20:38:11 -0400

    removed unused imports (for now)

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-import pymongo
-from dotenv import load_dotenv
 import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
